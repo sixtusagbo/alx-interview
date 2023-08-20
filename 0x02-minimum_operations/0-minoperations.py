4bbaa8a4c53305332ae1252a908fdb34c96720bf
Sixtus Agbo <miracleagbosixtus@gmail.com>
2023-08-20 14:34:34 +0100

    Minimum Operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+import math
+
+
+def minOperations(n: int) -> int:
+    """Return the sum of prime factors of a given number.
+
+    That is the minimum number of operations required to get the
+    exact number of `H` characters. Since at each point, n is divisible by that
+    particular number dividing it and the remainder will always be 0,
+    I used integer division to avoid type errors.
+    """
+    result = 0
+
+    # While n is divisble by 2
+    while n % 2 == 0:
+        # Divide n by 2
+        n = n // 2
+        # Add 2 to result
+        result += 2
+
+    # n is odd at this point
+    # Divide n repeatedly by i where i = i + 2 starting from 3
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        # While n is divisible by i
+        while n % i == 0:
+            # Divide n by i
+            n = n // i
+            # Add i to result
+            result += i
+
+    # If n is a prime number that is not 2, then n is greater than 2 at this point
+    if n > 2:
+        # Just add n to the result
+        result += n
+
+    return result
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,18 @@
+# Minimum Operations
+
+In a text file, there is a single character `H`. Your text editor can execute only two operations in this file: `Copy All` and `Paste`. Given a number `n`, write a method that calcu lates the fewest number of operations needed to result in exactly `n` `H` characters in the file.
+
+    - Prototype: `def minOperations(n)`
+    - Returns an integer
+    - If `n` is impossible to achieve, return `0`
+
+**Example:**
+
+`n = 9`
+
+`H` => `Copy All` => `Paste` => `HH` => `Paste` => `HHH` => `Copy All` => `Paste` => `HHHHHH` => `Paste` => `HHHHHHHHH`
+
+## Number of operations: `6`
+
+Min number of operations to reach 4 characters: 4
+Min number of operations to reach 12 characters: 7
