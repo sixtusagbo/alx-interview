085338631c9fcb0d18ea0abc204f32d12d074ee2
Sixtus Agbo <miracleagbosixtus@gmail.com>
2024-09-27 04:26:04 +0000

    Calculate perimeter of an island surrounded by water

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Island Perimeter """
+
+
+def island_perimeter(grid):
+    """Return the perimeter of the island described in grid"""
+    height = len(grid)
+    width = len(grid[0]) if height else 0
+    perimeter = 0
+
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                perimeter += 4
+
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
